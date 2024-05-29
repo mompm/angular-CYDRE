4f68bd9a9ac640bb24803544b5b9bb7db7056834
mompm <monabudeau@gmail.com>
2024-05-29 15:24:50 +0200

    ajout documentation

--- frontEnd/front/src/app/selecteurLangue/selecteurLangue.component.ts
+++ frontEnd/front/src/app/selecteurLangue/selecteurLangue.component.ts
@@ -1,5 +1,9 @@
 import { Component, ViewEncapsulation,Input } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 @Component({
@@ -14,7 +18,7 @@ export class SelecteurLangueComponent{
     selectLang:string="";
     transLang : string[] =[];
   
-    constructor(public translate: TranslateService){
+    constructor(public translate: TranslateService, public dialog : MatDialog){
       translate.setDefaultLang('FR');
       translate.addLangs(['EN', 'FR']);
       translate.use('FR');
@@ -33,4 +37,49 @@ export class SelecteurLangueComponent{
       this.getTransLanguage();
     }
 
+
+
+    openDialog() {
+      this.dialog.open(DocumentationPopup, {
+        width: '95%',
+        height: 'auto'
+      });
+    }
+  
   }
+
+    @Component({
+      selector: 'documentationPopup',
+      templateUrl: './documentationPopup.html',
+      styleUrls: ['./documentationPopup.scss'],
+      standalone: true,
+      imports: [MatTabsModule, CommonModule ],
+    })
+    export class DocumentationPopup {
+      dialogWidth: string = '95%';
+      dialogHeight: string = '75%';
+
+  updateDialogSize(tabIndex: number) {
+    switch (tabIndex) {
+      case 0:
+        this.dialogWidth = '95%';
+        this.dialogHeight = '200px';
+        break;
+      case 1:
+        this.dialogWidth = '95%';
+        this.dialogHeight = '300px';
+        break;
+      case 2:
+        this.dialogWidth = '95%';
+        this.dialogHeight = '400px';
+        break;
+      case 3:
+        this.dialogWidth = '95%';
+        this.dialogHeight = '500px';
+        break;
+      default:
+        this.dialogWidth = '95%';
+        this.dialogHeight = '200px';
+    }
+  }
+}
