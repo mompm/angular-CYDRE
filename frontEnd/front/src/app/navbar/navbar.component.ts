27da6b3ab40acaea1bf172d90f56870ad3325562
mompm <monabudeau@gmail.com>
2024-06-11 11:09:21 +0200

    frontend footer/navbar

--- frontEnd/front/src/app/navbar/navbar.component.ts
+++ frontEnd/front/src/app/navbar/navbar.component.ts
@@ -1,7 +1,12 @@
-import { Component,EventEmitter, OnInit, ViewEncapsulation, Output } from '@angular/core';
+import { Component,EventEmitter, OnInit, ViewEncapsulation, Output, Input } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
 import { AuthService } from '../service/auth.service';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
@@ -13,11 +18,26 @@ export class NavbarComponent {
   @Output() titleChange = new EventEmitter<string>();
   title : string = "modelingTitle";
   isLoggedIn! : boolean;
+  selectLang:string="";
+  transLang : string[] =[];
 
-  constructor(public authService : AuthService ){
+  constructor(public translate: TranslateService,public authService : AuthService, public dialog : MatDialog ){
+    translate.setDefaultLang('FR');
+    translate.addLangs(['EN', 'FR']);
+    translate.use('FR');
+    this.selectLang = translate.currentLang;
+  }
 
+    
+  setTransLanguage(){
+    this.translate.use(this.selectLang);
+  }
+
+  getTransLanguage(){
+    this.transLang=[...this.translate.getLangs()];
   }
   ngOnInit(){
+    this.getTransLanguage();
     this.authService.isLoggedIn.subscribe((status) => {
       this.isLoggedIn = status;
     });
@@ -56,5 +76,25 @@ export class NavbarComponent {
   }
   createAccount(){
     console.log("User clicked on fiche de create account page");
+  }
+
+  openDialog() {
+    this.dialog.open(DocumentationPopup, {});
+  }
+}
+
+
+@Component({
+  selector: 'documentationPopup',
+  templateUrl: './documentationPopup.html',
+  styleUrls: ['./documentationPopup.scss'],
+  standalone: true,
+  imports: [MatTabsModule, CommonModule, MatButtonModule],
+})
+export class DocumentationPopup {
+  constructor(public dialogRef: MatDialogRef<DocumentationPopup>) {}
+
+  onClose(): void {
+    this.dialogRef.close();
   }
 }
