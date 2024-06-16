bb9a2e26d22b2493ffbe0aa3469d59e7bb29f8ee
Nicolas Cornette <nicolas.cornette@univ-rennes1.fr>
2024-06-16 16:08:03 +0200

    Updating navbar and header (logo and font size, tabs alignment, tabs description)

--- frontEnd/front/src/app/header/header.component.ts
+++ frontEnd/front/src/app/header/header.component.ts
@@ -11,7 +11,7 @@ import { NavigationEnd, Router } from '@angular/router';
 export class HeaderComponent implements OnInit{
   //variable isLoggedIn est récuperer dans le component navbar 
   @Input() isLoggedIn!: boolean;
-  title : string = 'DefaultTitle';
+  subtitle : string = 'DefaultTitle';
 
   constructor(private router: Router) {}
 //les titres sont présents dans frontEnd/front/src/assets/i18n, ce sont les fichier EN.json et FR.json  
@@ -21,16 +21,16 @@ export class HeaderComponent implements OnInit{
         //vérifie la page avec url 
         const currentRoute = this.router.url;
         switch (currentRoute) {
-          case '/home': this.title = 'homeTitle'; break;
-          case '/ficheSite' : this.title = 'ficheSiteTitle'; break;
-          case '/settings': this.title = 'settingsTitle'; break;
-          case '/login': this.title = 'loginTitle'; break;
-          case '/modeling': this.title = 'modelingTitle'; break;
-          case '/simulator':this.title = 'simulatorTitle';break;
-          case '/analysis':this.title = 'analysisTitle';break;
-          case '/simulationHistory':this.title = 'Mes simulations';break;
+          case '/home': this.subtitle = 'homeTitle'; break;
+          case '/ficheSite' : this.subtitle = 'ficheSitesubTitle'; break;
+          case '/settings': this.subtitle = 'settingsTitle'; break;
+          case '/login': this.subtitle = 'loginTitle'; break;
+          case '/modeling': this.subtitle = 'modelingTitle'; break;
+          case '/simulator':this.subtitle = 'simulatorsubTitle';break;
+          case '/analysis':this.subtitle = 'analysisTitle';break;
+          case '/simulationHistory':this.subtitle = 'Mes simulations';break;
 
-          default: this.title = 'DefaultTitle'; break;
+          default: this.subtitle = 'DefaultTitle'; break;
         }
       }
     });
