bbf31445bbfc2b87901fa1d910148121c1a4fec2
Nicolas Cornette <nicolas.cornette@univ-rennes1.fr>
2024-06-21 16:31:32 +0200

    Updating run_cydre.

--- backEnd/launchers/run_cydre.py
+++ backEnd/launchers/run_cydre.py
@@ -9,8 +9,10 @@ Created on Thu Jun  1 16:06:06 2023
 import os
 import sys
 import time
+import pyproj
 import pandas as pd
 import geopandas as gpd
+from shapely.geometry import Point
 
 # Cydre modules
 from setup_cydre_path import setup_cydre_path
@@ -23,40 +25,74 @@ import utils.toolbox as toolbox
 
 start = time.time()
 
+SPATIAL_SIM = True
+
 
 #%% PREPARATION
-
-# Cydre results path
-output_path = "C:/Users/nicol/OneDrive - Université de Rennes/IR_CYDRE/figures/Projections"
-
-# Load the CSV file containing information about hydrological stations,
-# including national identifier, river name and coordinates.
-stations = pd.read_csv(os.path.join(app_root, 'data', 'stations.csv'), delimiter=';', encoding='ISO-8859-1')
-piezo_stations = gpd.read_file(os.path.join(app_root, 'data', 'piezometry', 'stations.shp'))
 
 # Initialize Cydre application, loading input parameters, datasets, etc.
 init = IN.Initialization(app_root)
 cydre_app = init.cydre_initialization()
+initial_date = init.params.getgroup("General").getparam("date").getvalue()
+data_path = os.path.join(app_root, 'data')
+hydro_path = os.path.join(data_path, 'hydrometry', 'discharge')
+surfex_path = os.path.join(data_path, 'climatic', 'surfex')
+piezo_path = os.path.join(data_path, 'piezometry')
+#output_path = "C:/Users/nicol/OneDrive - Université de Rennes/IR_CYDRE/figures/Projections"
+output_path = os.path.join(app_root, 'outputs', 'projections')
+
+# Hydrological stations
+stations = pd.read_csv(os.path.join(data_path, 'stations.csv'), delimiter=';', encoding='ISO-8859-1')
+lambert93_to_wgs84 = pyproj.Transformer.from_crs("EPSG:2154", "EPSG:4326", always_xy=True)
+x_wgs84, y_wgs84 = lambert93_to_wgs84.transform(stations["x_outlet"], stations["y_outlet"])
+geometry_stations = [Point(xy) for xy in zip(x_wgs84, y_wgs84)]
+gdf_stations = gpd.GeoDataFrame(stations, geometry=geometry_stations, crs="EPSG:4326")
+
+# Watersheds boundaries
+gdf_watersheds = gpd.read_file(os.path.join(data_path, 'watersheds.shp'))
+gdf_watersheds = gdf_watersheds.set_index('index')
+
+# Piezometric stations
+piezo_stations = pd.read_csv(os.path.join(app_root, 'data', 'piezometry', 'stations.csv'), delimiter=';', encoding='ISO-8859-1')
+geometry_piezometry = [Point(xy) for xy in zip(piezo_stations['X_WGS84'], piezo_stations['Y_WGS84'])]
+gdf_piezometry = gpd.GeoDataFrame(piezo_stations, geometry=geometry_piezometry, crs="EPSG:4326")
 
  # Watershed identifier
 watershed_id = cydre_app.UserConfiguration.user_watershed_id
-watershed_name = stations[stations['ID'] == watershed_id].name.values[0]
+watershed_name = stations[stations['ID'] == watershed_id].station_name.values[0]
 
 
 #%% CYDRE APPLICATION
 
 # Run the Cydre application
-cydre_app.run_spatial_similarity(spatial=True)
+if SPATIAL_SIM:
+    cydre_app.run_spatial_similarity()
+    
+    stations_names = []
+    
+    for ws in cydre_app.Similarity.similar_watersheds:
+        stations_names.append(gdf_stations[gdf_stations["ID"] == ws].station_name.values[0])
+        
+    print(f"Les bassins versants partageant la même typologie que {watershed_name} sont:")
+    for name in stations_names:
+        print(f"- {name}")
+    
 cydre_app.run_timeseries_similarity()
-cydre_app.select_scenarios(spatial=True)
+cydre_app.select_scenarios(spatial=False)
 df_streamflow_forecast, df_storage_forecast = cydre_app.streamflow_forecast()
 
 
 #%% VISUALIZATION AND RESULTS STORAGE
 baseflow_option = False
-results = OU.Outputs(cydre_app, output_path, watershed_name, stations, cydre_app.date, log=True,
+results = OU.Outputs(cydre_app, output_path, watershed_name, stations, initial_date, log=True,
                      module=True, baseflow=baseflow_option, options='viz_plotly')
-toolbox.save_object(results, os.path.join(output_path, watershed_name, cydre_app.date.strftime('%Y-%m-%d')), f'results_{watershed_name}')
+
+results.plot_typology_map(gdf_stations, gdf_watersheds, cydre_app.UserConfiguration.user_watershed_id)
+
+sys.exit()
+
+
+#toolbox.save_object(results, os.path.join(output_path, watershed_name, cydre_app.date.strftime('%Y-%m-%d')), f'results_{watershed_name}')
 
 
 #%% MODEL EVALUATION (ONLY FOR TEST MODE > COMPARISON WITH OBSERVATION ONLY IN THE PAST)
