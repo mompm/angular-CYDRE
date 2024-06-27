f4f21436ed3521b4157e783d7c4b603c5902c48e
mompm <monabudeau@gmail.com>
2024-06-27 11:36:38 +0200

    mise en place boutons + csv graphique

--- frontEnd/front/src/app/ficheSite/WaterTableDepthSeasonal/WaterTableDepthSeasonal.component.ts
+++ frontEnd/front/src/app/ficheSite/WaterTableDepthSeasonal/WaterTableDepthSeasonal.component.ts
@@ -52,6 +52,11 @@ function generateColors(numColors: number): string[] {
     months: string[] = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
     tickvals: string[] = this.months.map((month, index) => `${index + 1 < 10 ? '0' : ''}${index + 1}-01`);
     ticktext: string[] = this.months.map(month => month);
+    TabDepthByDaily: any[] = []; //tableau contenant toutes les données Depth triés
+    YearTabDepthByDaily: any[] = []; //tableau contenant les données Depth triés des années selectionné
+    lastUpdate: any | null;// dernière update des données des données Depth
+    resultArray: { key: string; values: number[]; q10?: number; q50?: number; q90?: number; }[] = []; // tableau contenant les quantiles 
+
     /**
      * 
      * @param dataService 
@@ -108,11 +113,11 @@ function generateColors(numColors: number): string[] {
    * Traite les données de décharge pour les rendre utilisables par l'hydrographe.
    * @returns Un objet contenant les données de décharge quotidiennes et par année, ainsi que la dernière mise à jour.
    */
-  processedDepth(): { TabDepthByDaily: any[], YearTabDepthByDaily: any[], lastUpdate: any } {
+  processedDepth(): { DepthByDaily: any[], YearDepthByDaily: any[], Update: any } {
     const targetYears: number[] = this.yearSelectionChange;
-    const TabDepthByDaily: any[] = [];
-    const YearTabDepthByDaily: any[] = [];
-    let lastUpdate = null;
+    const DepthByDaily: any[] = [];
+    const YearDepthByDaily: any[] = [];
+    let Update = null;
       // Vérification si this.dischargeStation est défini et non vide
       if (this.DataDepth && this.DataDepth.length > 0) {
         for (const entry of this.DataDepth) {
@@ -121,8 +126,8 @@ function generateColors(numColors: number): string[] {
           const day = new Date(entry.t).getDate(); // Récupérer le jour de la date
           const currentDate = new Date(entry.t);
 
-          if (!lastUpdate || currentDate > lastUpdate|| !isNaN(parseFloat(entry.t))) {
-            lastUpdate = currentDate; 
+          if (!Update || currentDate > Update|| !isNaN(parseFloat(entry.t))) {
+            Update = currentDate; 
           }
           // Formater le mois et le jour avec le format "mm-dd"
           const monthDay = `${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
@@ -139,33 +144,33 @@ function generateColors(numColors: number): string[] {
     
           // Ajouter cet objet au tableau de données traitées
           if (!isNaN(parseFloat(entry.d))) {
-            TabDepthByDaily.push(newDataEntry);
+            DepthByDaily.push(newDataEntry);
           }
 
           if (targetYears.includes(year)) {
-            YearTabDepthByDaily.push(newDataEntry);
+            YearDepthByDaily.push(newDataEntry);
           }
         }
       }
-    return { TabDepthByDaily, YearTabDepthByDaily, lastUpdate };
+    return { DepthByDaily, YearDepthByDaily, Update };
   }
 
     /**
    * Calcule les quantiles (10ème, 50ème, et 90ème) des données de décharge.
-   * @param TabDepthByDaily Tableau des données de profondeur quotidiennes.
+   * @param DepthByDaily Tableau des données de profondeur quotidiennes.
    * @returns Un objet contenant les quantiles calculés et les données groupées par jour.
    */
-    calculateQuantiles(TabDepthByDaily: any[]): { resultArray: { key: string; values: number[]; q10?: number; q50?: number; q90?: number; }[], q10: any, q50: any, q90: any } {
+    calculateQuantiles(DepthByDaily: any[]): { resultArray: { key: string; values: number[]; q10?: number; q50?: number; q90?: number; }[], q10: any, q50: any, q90: any } {
       const resultArray: { key: string; values: number[]; q10?: number; q50?: number; q90?: number; }[] = [];
       let q10: any;
       let q50: any;
       let q90: any;
 
-      from(TabDepthByDaily)
+      from(DepthByDaily)
       .pipe(
         filter(entry => !isNaN(parseFloat(entry.d))),
         groupBy(
-          TabDepthByDaily => TabDepthByDaily.daily,
+          DepthByDaily => DepthByDaily.daily,
           p => p.d
         ),
         mergeMap(group => zip(of(group.key), group.pipe(toArray())))
@@ -205,8 +210,12 @@ function generateColors(numColors: number): string[] {
     Depth_Seasonal() {
       const hyperlien = `https://ades.eaufrance.fr/Fiche/PtEau?Code=${this.DataDepth[1]}`;
         const targetYears: number[] = this.yearSelectionChange;
-        const  { TabDepthByDaily, YearTabDepthByDaily, lastUpdate } = this.processedDepth();
-        const { resultArray, q10, q50, q90 } = this.calculateQuantiles(TabDepthByDaily);
+        const  { DepthByDaily, YearDepthByDaily, Update } = this.processedDepth();
+        this.TabDepthByDaily = DepthByDaily;
+        this.YearTabDepthByDaily = YearDepthByDaily;
+        this.lastUpdate = Update;
+        const { resultArray, q10, q50, q90 } = this.calculateQuantiles(DepthByDaily);
+        this.resultArray = resultArray;
 
         const resultArraysKeys = resultArray.map(entry => entry.key);
         const variabilityX = resultArraysKeys.concat(resultArraysKeys.slice().reverse());
@@ -297,8 +306,8 @@ function generateColors(numColors: number): string[] {
             legend: { orientation: "h", yanchor: "top", y: 1.1, xanchor: "right", x: 1 }
         }
     };
-        const startYear = TabDepthByDaily.length > 0 ? TabDepthByDaily[1].years : 'N/A';
-        const endYear = TabDepthByDaily.length > 0 ? TabDepthByDaily[TabDepthByDaily.length - 1].years : 'N/A';
+        const startYear = this.TabDepthByDaily.length > 0 ? this.TabDepthByDaily[1].years : 'N/A';
+        const endYear = this.TabDepthByDaily.length > 0 ? this.TabDepthByDaily[this.TabDepthByDaily.length - 1].years : 'N/A';
         // Construction du libellé pour la moyenne
         const labelmedian = `moyenne [${startYear} - ${endYear}]`;
         const labelinvariant = `variabilité [${startYear} - ${endYear}]`;
@@ -328,7 +337,7 @@ function generateColors(numColors: number): string[] {
             // Boucle sur les événements
       for (let i = 0; i < targetYears.length; i++) {
         const year = targetYears[i];
-        const df_event = YearTabDepthByDaily.filter(item => item.years === year);
+        const df_event = this.YearTabDepthByDaily.filter(item => item.years === year);
         if (df_event) {
           const trace = {
             x: df_event.map(item => item.daily),
@@ -345,8 +354,8 @@ function generateColors(numColors: number): string[] {
       }
       
         // Mettre à jour l'annotation pour afficher la date de mise à jour actuelle
-        if (lastUpdate){       
-          const currentDate = `${lastUpdate.getDate().toString().padStart(2, '0')}-${(lastUpdate.getMonth() + 1).toString().padStart(2, '0')}-${lastUpdate.getFullYear()}`;
+        if (this.lastUpdate){       
+          const currentDate = `${this.lastUpdate.getDate().toString().padStart(2, '0')}-${(this.lastUpdate.getMonth() + 1).toString().padStart(2, '0')}-${this.lastUpdate.getFullYear()}`;
           const updatedAnnotation = this.fig.layout.annotations.find((annotation: any) => annotation.text.includes('Mis à jour le :'));
           if (updatedAnnotation) {
             updatedAnnotation.text = `Mis à jour le : ${currentDate}`;
@@ -360,6 +369,91 @@ function generateColors(numColors: number): string[] {
         
       
       }
+  
+      downloadCSV(){
+        console.log(this.YearTabDepthByDaily)
+        // Initialiser un objet pour stocker les données fusionnées
+        const mergedData: { [key: string]: { [year: string]: number | null, q10: number | null ,q50: number | null, q90: number | null} } = {};
+        // Initialiser les années présentes dans yearTabDepth
+        const years = this.yearSelectionChange;  
+        // Ajouter les données de resultArray
+        for (const result of this.resultArray) {
+            mergedData[result.key] = {
+              q10: result.q10 !== undefined ? result.q10 : null,
+              q50: result.q50 !== undefined ? result.q50 : null,
+              q90: result.q90 !== undefined ? result.q90 : null,
+            };
+        }
+    
+        // Ajouter les données de yearTabDepth
+        for (const entry of this.YearTabDepthByDaily) {
+            mergedData[entry.daily][`${entry.years}`] = entry.d;
+        }
+                              
+          // Initialisation de la chaîne CSV avec l'en-tête initial
+          let csv = 'Date,Q90,Q50,Q10';
+  
+          // Ajouter les années à l'en-tête CSV
+          this.yearSelectionChange.forEach(year => {
+              csv += `,${year}`;
+          });
+  
+          // Ajouter une nouvelle ligne pour le CSV
+          csv += '\n';
+  
+          // Parcours de mergedData pour construire le CSV
+          Object.keys(mergedData).forEach(date => {
+              // Reformater la date au format souhaité si nécessaire
+              const formattedDate = date; // Assurez-vous de reformater correctement la date si nécessaire
+  
+              // Extraire les valeurs q10, q50, q90
+              const q10 = mergedData[date]['q10'] !== null ? mergedData[date]['q10'] : '';
+              const q50 = mergedData[date]['q50'] !== null ? mergedData[date]['q50'] : '';
+              const q90 = mergedData[date]['q90'] !== null ? mergedData[date]['q90'] : '';
+  
+              // Construire la ligne CSV pour chaque date
+              let csvLine = `${formattedDate},${q90},${q50},${q10}`;
+  
+              // Ajouter les données spécifiques aux années
+              years.forEach(year => {
+                  const value = mergedData[date][year] !== null ? mergedData[date][year] : '';
+                  csvLine += `,${value}`;
+              });
+  
+              // Ajouter une nouvelle ligne pour le CSV
+              csvLine += '\n';
+  
+              // Ajouter la ligne au CSV final
+              csv += csvLine;
+          });
+  
+    
+              // Créer le Blob à partir du CSV
+              const blob = new Blob([csv], { type: 'text/csv' });
+    
+              // Créer l'URL du Blob
+              const url = window.URL.createObjectURL(blob);
+  
+              const fileName = `profondeur_de_la_nappe[${this.DataDepth[1]}].csv`;
+    
+              // Créer un élément <a> pour le téléchargement du fichier
+              const a = document.createElement('a');
+              a.href = url;
+              a.download = fileName;
+    
+              // Ajouter l'élément <a> au corps du document
+              document.body.appendChild(a);
+    
+              // Simuler un clic sur le lien pour déclencher le téléchargement
+              a.click();
+    
+              // Supprimer l'élément <a> du corps du document
+              document.body.removeChild(a);
+    
+              // Révoquer l'URL du Blob pour libérer la mémoire
+              window.URL.revokeObjectURL(url);
+              
+          }
   
 
 
--- frontEnd/front/src/app/ficheSite/temperatureSeasonal/temperatureSeasonal.component.ts
+++ frontEnd/front/src/app/ficheSite/temperatureSeasonal/temperatureSeasonal.component.ts
@@ -48,6 +48,10 @@ function generateColors(numColors: number): string[] {
     months: string[] = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]; // Mois de l'année
     tickvals: string[] = this.months.map((month, index) => `${index + 1 < 10 ? '0' : ''}${index + 1}-01`); // Valeurs des ticks pour l'axe x
     ticktext: string[] = this.months.map(month => month); // Texte des ticks pour l'axe x
+    TabTemperatureByDaily: any[] = []; //tableau contenant toutes les données Temperature triés
+    YearTabTemperatureByDaily: any[] = []; //tableau contenant les données Temperature triés des années selectionné
+    lastUpdate: any | null;// dernière update des données des données Temperature
+    resultArray: { key: string; values: number[]; q10?: number; q50?: number; q90?: number; }[] = []; // tableau contenant les quantiles 
 
 
     /**
@@ -106,11 +110,11 @@ function generateColors(numColors: number): string[] {
      * 
      * @returns Un objet contenant les données de temperature journalières et annuelles ainsi que la dernière date de mise à jour
      */
-    processedTemperature(): { TabTemperatureByDaily: any[], YearTabTemperatureByDaily: any[], lastUpdate: any } {
+    processedTemperature(): { TemperatureByDaily: any[], YearTemperatureByDaily: any[], Update: any } {
         const targetYears: number[] = this.yearSelectionChange;
-        const TabTemperatureByDaily: any[] = [];
-        const YearTabTemperatureByDaily: any[] = [];
-        let lastUpdate = null;
+        const TemperatureByDaily: any[] = [];
+        const YearTemperatureByDaily: any[] = [];
+        let Update = null;
          // Vérification si this.dischargeStation est défini et non vide
          if (this.DataTemperature && this.DataTemperature.length > 0) {
           for (const entry of this.DataTemperature) {
@@ -119,8 +123,8 @@ function generateColors(numColors: number): string[] {
             const day = new Date(entry.t).getDate(); // Récupérer le jour de la date
             const currentDate = new Date(entry.t);
   
-            if (!lastUpdate || currentDate > lastUpdate|| !isNaN(parseFloat(entry.t))) {
-              lastUpdate = currentDate; 
+            if (!Update || currentDate > Update|| !isNaN(parseFloat(entry.t))) {
+              Update = currentDate; 
             }
             // Formater le mois et le jour avec le format "mm-dd"
             const monthDay = `${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
@@ -135,34 +139,34 @@ function generateColors(numColors: number): string[] {
       
             // Ajouter cet objet au tableau de données traitées
             if (!isNaN(parseFloat(entry.Q))) {
-              TabTemperatureByDaily.push(newDataEntry);
+              TemperatureByDaily.push(newDataEntry);
             }
   
             if (targetYears.includes(year)) {
-              YearTabTemperatureByDaily.push(newDataEntry);
+              YearTemperatureByDaily.push(newDataEntry);
             }
           }
         } 
-        return{TabTemperatureByDaily, YearTabTemperatureByDaily, lastUpdate}
+        return{TemperatureByDaily, YearTemperatureByDaily, Update}
       }
 
         /**
    * Calcule les quantiles (10%, 50%, 90%) des précipitations journalières cumulées.
    * 
-   * @param  TabTemperatureByDaily Tableau des températures journalières cumulées
+   * @param  TemperatureByDaily Tableau des températures journalières cumulées
    * @returns Un objet contenant les quantiles calculés pour chaque jour et les quantiles globaux
    */
-  calculateQuantiles(TabTemperatureByDaily: any[]): { resultArray: { key: string; values: number[]; q10?: number; q50?: number; q90?: number; }[], q10: any, q50: any, q90: any } {
+  calculateQuantiles(TemperatureByDaily: any[]): { resultArray: { key: string; values: number[]; q10?: number; q50?: number; q90?: number; }[], q10: any, q50: any, q90: any } {
     const resultArray: { key: string; values: number[]; q10?: number; q50?: number; q90?: number; }[] = [];
     let q10: any;
     let q50: any;
     let q90: any;
 
-    from(TabTemperatureByDaily)
+    from(TemperatureByDaily)
     .pipe(
       filter(entry => !isNaN(parseFloat(entry.Q))),
       groupBy(
-        TabTemperatureByDaily => TabTemperatureByDaily.daily,
+        TemperatureByDaily => TemperatureByDaily.daily,
         p => p.Q
       ),
       mergeMap(group => zip(of(group.key), group.pipe(toArray())))
@@ -202,9 +206,12 @@ function generateColors(numColors: number): string[] {
    */
     temperature_Seasonal() {
         const targetYears: number[] = this.yearSelectionChange;
-        const  {TabTemperatureByDaily, YearTabTemperatureByDaily, lastUpdate} = this.processedTemperature();
-        const {resultArray, q10, q50, q90 } = this.calculateQuantiles(TabTemperatureByDaily);
-
+        const  {TemperatureByDaily, YearTemperatureByDaily, Update} = this.processedTemperature();
+        this.TabTemperatureByDaily = TemperatureByDaily;
+        this.YearTabTemperatureByDaily = YearTemperatureByDaily;
+        this.lastUpdate = Update;
+        const {resultArray, q10, q50, q90 } = this.calculateQuantiles(TemperatureByDaily);
+        this.resultArray = resultArray;
         const resultArraysKeys = resultArray.map(entry => entry.key);
         const variabilityX = resultArraysKeys.concat(resultArraysKeys.slice().reverse());
         const resultArrayq10 = resultArray.map(entry => entry.q10);
@@ -244,8 +251,8 @@ function generateColors(numColors: number): string[] {
             legend: { orientation: "h", yanchor: "top", y: 1.1, xanchor: "right", x: 1 }
         }
     };
-        const startYear = TabTemperatureByDaily.length > 0 ? TabTemperatureByDaily[1].years : 'N/A';
-        const endYear = TabTemperatureByDaily.length > 0 ? TabTemperatureByDaily[TabTemperatureByDaily.length - 1].years : 'N/A';
+        const startYear = this.TabTemperatureByDaily.length > 0 ? this.TabTemperatureByDaily[1].years : 'N/A';
+        const endYear = this.TabTemperatureByDaily.length > 0 ? this.TabTemperatureByDaily[this.TabTemperatureByDaily.length - 1].years : 'N/A';
         // Construction du libellé pour la moyenne
         const labelmedian = `moyenne [${startYear} - ${endYear}]`;
         const labelinvariant = `variabilité [${startYear} - ${endYear}]`;
@@ -275,7 +282,7 @@ function generateColors(numColors: number): string[] {
             // Boucle sur les événements
       for (let i = 0; i < targetYears.length; i++) {
         const year = targetYears[i];
-        const df_event = YearTabTemperatureByDaily.filter(item => item.years === year);
+        const df_event = this.YearTabTemperatureByDaily.filter(item => item.years === year);
         if (df_event) {
           const trace = {
             x: df_event.map(item => item.daily),
@@ -292,8 +299,8 @@ function generateColors(numColors: number): string[] {
       }
       
         // Mettre à jour l'annotation pour afficher la date de mise à jour actuelle
-        if (lastUpdate){       
-          const currentDate = `${lastUpdate.getDate().toString().padStart(2, '0')}-${(lastUpdate.getMonth() + 1).toString().padStart(2, '0')}-${lastUpdate.getFullYear()}`;
+        if (this.lastUpdate){       
+          const currentDate = `${this.lastUpdate.getDate().toString().padStart(2, '0')}-${(this.lastUpdate.getMonth() + 1).toString().padStart(2, '0')}-${this.lastUpdate.getFullYear()}`;
           const updatedAnnotation = this.fig.layout.annotations.find((annotation: any) => annotation.text.includes('Mis à jour le :'));
           if (updatedAnnotation) {
             updatedAnnotation.text = `Mis à jour le : ${currentDate}`;
@@ -307,6 +314,92 @@ function generateColors(numColors: number): string[] {
         
       
       }
+
+      downloadCSV(){
+        console.log(this.YearTabTemperatureByDaily)
+        // Initialiser un objet pour stocker les données fusionnées
+        const mergedData: { [key: string]: { [year: string]: number | null, q10: number | null ,q50: number | null, q90: number | null} } = {};
+        // Initialiser les années présentes dans yearTabTemperature
+        const years = this.yearSelectionChange;  
+        // Ajouter les données de resultArray
+        for (const result of this.resultArray) {
+            mergedData[result.key] = {
+              q10: result.q10 !== undefined ? result.q10 : null,
+              q50: result.q50 !== undefined ? result.q50 : null,
+              q90: result.q90 !== undefined ? result.q90 : null,
+            };
+        }
+    
+        // Ajouter les données de yearTabTemperature
+        for (const entry of this.YearTabTemperatureByDaily) {
+            mergedData[entry.daily][`${entry.years}`] = entry.Q;
+        }
+                              
+          // Initialisation de la chaîne CSV avec l'en-tête initial
+          let csv = 'Date,Q90,Q50,Q10';
+  
+          // Ajouter les années à l'en-tête CSV
+          this.yearSelectionChange.forEach(year => {
+              csv += `,${year}`;
+          });
+  
+          // Ajouter une nouvelle ligne pour le CSV
+          csv += '\n';
+  
+          // Parcours de mergedData pour construire le CSV
+          Object.keys(mergedData).forEach(date => {
+              // Reformater la date au format souhaité si nécessaire
+              const formattedDate = date; // Assurez-vous de reformater correctement la date si nécessaire
+  
+              // Extraire les valeurs q10, q50, q90
+              const q10 = mergedData[date]['q10'] !== null ? mergedData[date]['q10'] : '';
+              const q50 = mergedData[date]['q50'] !== null ? mergedData[date]['q50'] : '';
+              const q90 = mergedData[date]['q90'] !== null ? mergedData[date]['q90'] : '';
+  
+              // Construire la ligne CSV pour chaque date
+              let csvLine = `${formattedDate},${q90},${q50},${q10}`;
+  
+              // Ajouter les données spécifiques aux années
+              years.forEach(year => {
+                  const value = mergedData[date][year] !== null ? mergedData[date][year] : '';
+                  csvLine += `,${value}`;
+              });
+  
+              // Ajouter une nouvelle ligne pour le CSV
+              csvLine += '\n';
+  
+              // Ajouter la ligne au CSV final
+              csv += csvLine;
+          });
+  
+    
+              // Créer le Blob à partir du CSV
+              const blob = new Blob([csv], { type: 'text/csv' });
+    
+              // Créer l'URL du Blob
+              const url = window.URL.createObjectURL(blob);
+  
+              const fileName = `temperature.csv`;
+    
+              // Créer un élément <a> pour le téléchargement du fichier
+              const a = document.createElement('a');
+              a.href = url;
+              a.download = fileName;
+    
+              // Ajouter l'élément <a> au corps du document
+              document.body.appendChild(a);
+    
+              // Simuler un clic sur le lien pour déclencher le téléchargement
+              a.click();
+    
+              // Supprimer l'élément <a> du corps du document
+              document.body.removeChild(a);
+    
+              // Révoquer l'URL du Blob pour libérer la mémoire
+              window.URL.revokeObjectURL(url);
+              
+          }
+  
   
 
 
