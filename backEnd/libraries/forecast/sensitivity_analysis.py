8096dfd3a83542400dab90659ff1e6f4e6bbce69
Jean-Raynald de Dreuzy <Jean-Raynald.de-Dreuzy@univ-rennes.fr>
2024-07-24 09:21:36 +0200

    add comments

--- backEnd/libraries/forecast/sensitivity_analysis.py
+++ backEnd/libraries/forecast/sensitivity_analysis.py
@@ -10,6 +10,28 @@ import itertools
 
 
 class SensitivityAnalysis():
+    """
+    Attributes
+    ----------
+    param_names : list of strings
+        Name of parameters, should correspond to parameter names in xml files
+    param_ranges : list of different format 
+        Range of parameters with either 
+            - values
+            - ranges
+    num_values : list of integers
+        Number of occurences for a given parameter
+    param_values : list of values
+        Values for each of the parameters
+    
+    Methods 
+    -------
+    set_parameters_values(self):
+        Set parameter values with the two possibilities
+    set_parameters_combination(self):
+        Cartesian product of parameters
+        
+    """
     
     def __init__(self, param_names, param_ranges, num_values):
         
@@ -19,6 +41,11 @@ class SensitivityAnalysis():
         
         
     def set_parameters_values(self):
+        """ 
+        Set parameter values with the two possibilities
+        - range of values and number of values: systematic sampling
+        - list of values already put in parameters 
+        """
         
         self.param_values = []
         
@@ -26,18 +53,28 @@ class SensitivityAnalysis():
             
             # int or float (linspace values)
             if isinstance(self.param_ranges[i[0]], tuple):
+                # tuple: 2 values (..., ...)
                 min_value = i[1][0]
                 max_value = i[1][1]
                 values = np.linspace(min_value, max_value, self.num_values[i[0]])
                 
             # string or defined values
             elif isinstance(self.param_ranges[i[0]], list):
+                # list: [...,...]
                 values = self.param_ranges[i[0]]
         
             self.param_values.append(values)
       
     
     def set_parameters_combination(self):
+        """
+        Cartesian product of parameters 
+        
+        Returns
+        -------
+        param_combinations : list of dictionary of 'zipped' parameters 
+            list of parameters for each of the simulations 
+        """
 
         combinations = list(itertools.product(*self.param_values))
 
