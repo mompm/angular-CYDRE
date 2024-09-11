8a92690ae271466f04a84a42c593b4001828434c
Nicolas Cornette <nicolas.cornette@univ-rennes1.fr>
2024-09-11 11:46:55 +0200

    Création d'un composant pour la carte de la typologie des BV + popup d'info améliorée + MAJ typo des stations

--- frontEnd/front/src/app/ficheSite/ficheSite.component.ts
+++ frontEnd/front/src/app/ficheSite/ficheSite.component.ts
@@ -106,6 +106,28 @@ import * as Plotly from 'plotly.js-dist';
     openDialogLoc() {
       this.dialog.open(PopupDialogLoc);
     }
+
+    openDialogClassif(event: MouseEvent) {
+      const targetElement = event.target as HTMLElement;
+      const rect = targetElement.getBoundingClientRect(); // Récupère la position du bouton
+
+      console.log('Position du bouton :', rect);
+
+      this.dialog.open(popupDialogClassif, {
+        width: '400px',   // Largeur de la fenêtre
+        maxHeight: '80vh', // Limite la hauteur pour éviter qu'elle déborde
+        panelClass: 'custom-dialog-container', // Classe personnalisée pour ajuster les styles
+        hasBackdrop: true,
+        backdropClass: 'custom-backdrop', // Ajout de la classe backdrop personnalisée
+        autoFocus: true,
+        position: {
+          top: `${rect.bottom - 50}px`, // 10px en dessous du bouton
+          left: `${rect.right + 20}px`,   // 5px à droite du bouton
+          right: 'auto',                 // Laisser l'alignement horizontal au contenu
+          bottom: 'auto'  
+        }
+      });
+    }
     
     /**
      * 
@@ -194,272 +216,28 @@ import * as Plotly from 'plotly.js-dist';
       });
     }
 
-    testt = true
-    ngDoCheck() { 
-      if(this.DataGDFStation.length > 0 && this.DataGDFWatershed.length >0 && this.testt){
-        //this.test()
-        this.initializeMap();
-        this.testt = false;
-      }
-    }
-    
-
-    test(){
-      let gdf_stations_filter : any[] = [];
-      const gdf_watersheds_filter : any[]= [];
-      
-      for(let key in this.DataGDFStation){
-        if(this.DataGDFStation[key].typology != 99){
-          gdf_stations_filter.push(this.DataGDFStation[key]);
-        }
-      }
-      console.log(gdf_stations_filter);
-     //gdf_stations_filter = this.DataGDFStation;
-      for (let key in this.DataGDFWatershed){
-        for(let keystation in gdf_stations_filter){
-          if(this.DataGDFWatershed[key].index == gdf_stations_filter[keystation].index){
-            let watershedCopy = {
-              ...this.DataGDFWatershed[key],
-              typology: gdf_stations_filter[keystation].typology
-          };
-          // Ajout de l'objet modifié à gdf_watersheds_filter
-          gdf_watersheds_filter.push(watershedCopy);
-          }
-        }
-      }
-      const colors: { [key: number]: string } = {
-        0: '#1f77b4',   // Bleu
-        1: '#ff7f0e',   // Orange
-        2: '#2ca02c',   // Vert
-        3: '#d62728',   // Rouge
-        4: '#9467bd',   // Violet
-        5: '#8c564b',   // Marron
-        6: '#e377c2',   // Rose
-      };
-
-      const typologyNames: { [key: number]: string } = {
-        0: '0',
-        1: '1',
-        2: '2',
-        3: '3',
-        4: '4',
-        5: '5',
-        6: '6',
-    };
-        const x: any[] = [];
-        const y: any[] = [];
-        const colorsfill: any[] = [];
-        const hovertexts: string[] = [];
-        for (let i = 0; i < gdf_stations_filter.length; i++) {
-          x.push(Number(gdf_stations_filter[i].x_outlet)); 
-          y.push(Number(gdf_stations_filter[i].y_outlet));
-          let typology = Number(gdf_stations_filter[i].typology);
-          let fill = colors[typology];  // Récupère la couleur selon la typologie
-          colorsfill.push(fill);
-          let hovertext = `
-            <b>Identifiant:</b> ${gdf_stations_filter[i].index}<br>
-            <b>Nom de la station hydrologique:</b> ${gdf_stations_filter[i].station_name}<br>
-            <b>Groupe:</b> ${gdf_stations_filter[i].typology}`;
-        hovertexts.push(hovertext);
-        }
-
-        
-        const figData: any[] = [];
-        Object.keys(colors).forEach((key) => {
-          figData.push({
-              type: 'scattermapbox',
-              lon: [0,0,0,0,0,0,0,0,0,0,0],  
-              lat: [0,0,0,0,0,0,0,0,0,0,0],
-              mode: 'markers',
-              marker: {
-                  color: colors[Number(key)],
-                  size: 10,
-              },
-              showlegend: true,
-              name: typologyNames[key as any],  // Utilisez le nom de la typologie pour la légende
-          });
-      });
-        const figLayout = {
-            mapbox: {
-                style: 'carto-positron',
-                center: { lat: 48.2141667, lon: -2.9424167 },
-                zoom: 7,
-            },
-            paper_bgcolor: 'rgba(0,0,0,0)',
-            margin: { l: 0, r: 0, t: 0, b: 0 },
-            showlegend: true,
-            legend: {
-              title: {
-                  text: 'Légende des Typologies',
-                  font: {
-                      size: 12,
-                      color: '#000'  // Couleur du texte du titre
-                  }
-              },
-            }
-            
-        };
-      const watershedPolygons = this.DataGDFWatershed.map((data, index) => {
-          const polygonCoords = data.geometry.coordinates[0];
-          return {
-              type: 'scattermapbox',
-              lon: polygonCoords.map((coord: number[]) => coord[0]),
-              lat: polygonCoords.map((coord: number[]) => coord[1]),
-              mode: 'lines',
-              line: { width: 0.8, color: '#3E88A6' },
-              fill: 'toself',
-              fillcolor: 'transparent',
-              hoverinfo: 'none',
-              name: `Watershed Boundaries ${index + 1}`,
-              showlegend: false,
-          };
-      });
-      figData.push(...watershedPolygons);
-
-        figData.push({
-          type: 'scattermapbox',
-          lon: x,
-          lat: y,
-          mode: 'markers',
-          marker: {
-              color: colorsfill,
-              opacity: 0.8,  // Ajustez l'opacité au besoin
-              size: 10,      // Ajustez la taille des marqueurs au besoin
-          },
-          hoverinfo: 'text',
-          hovertext: hovertexts,
-          name: '',
-          showlegend: false,
-      });
-        Plotly.newPlot('mappy', figData, figLayout);
-    }
 
 
-
-    initializeMap() {
-      let gdf_stations_filter : any[] = [];
-      const gdf_watersheds_filter : any[]= [];
-      
-      for(let key in this.DataGDFStation){
-        if(this.DataGDFStation[key].typology != 99){
-          gdf_stations_filter.push(this.DataGDFStation[key]);
-        }
-      }
-
-      for (let key in this.DataGDFWatershed){
-        for(let keystation in gdf_stations_filter){
-          if(this.DataGDFWatershed[key].index == gdf_stations_filter[keystation].index){
-            let watershedCopy = {
-              ...this.DataGDFWatershed[key],
-              typology: gdf_stations_filter[keystation].typology
-          };
-          // Ajout de l'objet modifié à gdf_watersheds_filter
-          gdf_watersheds_filter.push(watershedCopy);
-          }
-        }
-      }
-
-      const WatLayers = {
-        "ligthMap" : L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }),
-      };
-      if (this.mapy) {
-        this.mapy.remove();
-      }
-      this.mapy = L.map('mapy', {
-        center: [48.2141667, -2.9424167],
-        zoomControl: true,
-        zoom: 7.5,
-      layers: [WatLayers['ligthMap']]
-    });
-
-    L.control.scale({ maxWidth: 100, imperial: false }).addTo(this.mapy);
-    this.mapy.attributionControl.remove();
-
-    //création des polygones de toutes les stations
-      //boucle pour parcourir toutes les stations
-      gdf_watersheds_filter.forEach((data, index) => {
-        //récupère les coordonées du polygones
-        const polygonCoords= data.geometry.coordinates[0].map((coord: any[]) => [coord[1], coord[0]]);
-        //ajout du polygone
-        L.polyline(polygonCoords, {
-          color: '#3E88A6',
-          weight: 0.8,
-          fill : false,
-          opacity: 1
-        }).addTo(this.mapy);
-      });
-
-      const colors: { [key: number]: string } = {
-        0: '#1f77b4',   // Bleu
-        1: '#ff7f0e',   // Orange
-        2: '#2ca02c',   // Vert
-        3: '#d62728',   // Rouge
-        4: '#9467bd',   // Violet
-        5: '#8c564b',   // Marron
-        6: '#e377c2',   // Rose
-      };
-
-      const typologyNames: { [key: number]: string } = {
-        0: '0',
-        1: '1',
-        2: '2',
-        3: '3',
-        4: '4',
-        5: '5',
-        6: '6',
-      };
-
-      gdf_stations_filter.forEach(station => {
-        let typology = Number(station.typology) as keyof typeof colors;
-        let fillColor = colors[typology];
-        let marker = L.circleMarker([station.y_outlet, station.x_outlet], {
-          radius: 5,
-          fillColor: fillColor,
-          color: fillColor,
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.8
-        }).addTo(this.mapy);
-
-        marker.bindTooltip(`
-          <b>ID:</b> ${station.index}<br>
-          <b>Nom:</b> ${station.station_name}<br>
-          <b>Groupe:</b> ${station.typology}
-        `, {
-          direction: 'top',
-          permanent: false,
-          sticky: true
-        });
-    
-        // marker.bindPopup(`
-        //   <b>ID:</b> ${station.index}<br>
-        //   <b>Nom:</b> ${station.station_name}<br>
-        //   <b>Groupe:</b> ${station.typology}
-        // `);
-      });
-
- // Ajout de la légende dans la div HTML
- const legendItemsContainer = document.getElementById('legend-items');
- if (legendItemsContainer) {
-   const grades = Object.keys(colors) as unknown as (keyof typeof colors)[];
-   console.log(grades);
-   grades.forEach(grade => {
-     const legendItem = document.createElement('div');
-     legendItem.className = 'legend-item';
-     legendItem.innerHTML = `
-        <div style ="display: flex; align-items: center;">
-       <div style="width: 10px; height: 10px; background-color:${colors[grade]}; border-radius: 50%; margin-right: 10px"></div>
-       <div>${typologyNames[grade]}</div>
-       </div>
-     `;
-     legendItemsContainer.appendChild(legendItem);
-   });
- }
+//  // Ajout de la légende dans la div HTML
+//  const legendItemsContainer = document.getElementById('legend-items');
+//  if (legendItemsContainer) {
+//    const grades = Object.keys(colors) as unknown as (keyof typeof colors)[];
+//    console.log(grades);
+//    grades.forEach(grade => {
+//      const legendItem = document.createElement('div');
+//      legendItem.className = 'legend-item';
+//      legendItem.innerHTML = `
+//         <div style ="display: flex; align-items: center;">
+//        <div style="width: 10px; height: 10px; background-color:${colors[grade]}; border-radius: 50%; margin-right: 10px"></div>
+//        <div>${typologyNames[grade]}</div>
+//        </div>
+//      `;
+//      legendItemsContainer.appendChild(legendItem);
+//    });
+//  }
 }
 
-  }
+  // }
   /**
    * 
    */
@@ -476,3 +254,9 @@ import * as Plotly from 'plotly.js-dist';
     templateUrl : './popupDialogLoc.html',
   })
   export class PopupDialogLoc {}
+
+  @Component({
+    selector: 'popupDialogClassif',
+    templateUrl : './popupDialogClassif.html',
+  })
+  export class popupDialogClassif {}
---
+++ frontEnd/front/src/app/ficheSite/typology-map/typology-map.component.ts
@@ -0,0 +1,139 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-typology-map',
+  templateUrl: './typology-map.component.html',
+  styleUrls: ['./typology-map.component.scss']
+})
+export class TypologyMapComponent implements OnInit, OnChanges {
+
+  @Input() selectedWatershedID: string | null = null;
+  @Input() DataGDFStation: any[] = [];
+  @Input() DataGDFWatershed: any[] = [];
+
+  private map: any;
+
+  ngOnInit(): void {
+    this.initializeMap();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selectedWatershedID'] || changes['DataGDFStation'] || changes['DataGDFWatershed']) {
+      this.updateMap();
+    }
+  }
+
+  initializeMap(): void {
+    this.map = L.map('mapy', {
+      center: [48.2141667, -2.9424167],
+      zoom: 7.5,
+      layers: [L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      })]
+    });
+
+    L.control.scale({ maxWidth: 100, imperial: false }).addTo(this.map);
+    this.map.attributionControl.remove();
+
+    this.updateMap();
+  }
+
+  updateMap(): void {
+    if (!this.map || !this.DataGDFStation.length || !this.DataGDFWatershed.length) {
+      console.log('Map or data not ready');
+      return;
+    }
+
+    // Nettoyage des anciens polygones
+    this.map.eachLayer((layer: any) => {
+      if (layer instanceof L.Polygon || layer instanceof L.Polyline) {
+        this.map.removeLayer(layer);
+      }
+    });
+
+    // Filtrage des stations
+    const gdf_stations_filter = this.DataGDFStation.filter(station => station.typology != 99);
+    
+    // Création des polygones filtrés
+    const gdf_watersheds_filter = this.DataGDFWatershed
+      .filter(watershed => gdf_stations_filter.some(station => station.index === watershed.index))
+      .map(watershed => {
+        const matchingStation = gdf_stations_filter.find(station => station.index === watershed.index);
+        return {
+          ...watershed,
+          typology: matchingStation ? matchingStation.typology : null
+        };
+      });
+
+    // Définir les couleurs pour les typologies
+    const colors: { [key: number]: string } = {
+      0: '#0000FF',
+      1: '#FF0000',
+      2: '#FFFF66',
+      3: '#3399FF',
+      4: '#66FF99',
+      5: '#FFCC66',
+      6: '#99FF66'
+    };
+
+    // Récupère la typologie de la station sélectionnée
+    const selectedTypology = this.DataGDFStation.find(station => station.index === this.selectedWatershedID)?.typology;
+    console.log('Selected Typology:', selectedTypology);
+
+    // Trouver les stations du même groupe que la sélection
+    const selectedStations = gdf_stations_filter.filter(station => station.typology === selectedTypology);
+    console.log('quelles stations?', selectedStations)
+
+    const stationsList = document.getElementById('stations-list');
+    if (stationsList) {
+      stationsList.innerHTML = selectedStations.map(station => `<li>${station.station_name}</li>`).join('');
+    }
+    console.log('Liste des stations:', stationsList)
+
+
+    // Création des polygones pour les bassins versants
+    gdf_watersheds_filter.forEach(data => {
+      const typology = Number(data.typology) as keyof typeof colors;
+      const fillColor = colors[typology] || '#CCCCCC'; // Couleur par défaut si typology non définie
+      const opacity = selectedTypology !== null && selectedTypology !== typology ? 0.05 : 0.8;
+
+      console.log('Adding polygon with fillColor:', fillColor, 'and opacity:', opacity);
+
+      // Récupère les coordonnées du polygone
+      const polygonCoords = data.geometry.coordinates[0].map((coord: any[]) => [coord[1], coord[0]]);
+      
+
+      // Ajoute le polygone à la carte avec la couleur appropriée
+      L.polygon(polygonCoords, {
+        color: 'black',
+        weight: 0.5,
+        fillColor: fillColor,
+        fillOpacity: opacity,
+      })
+      .bindTooltip(data.name, {
+        permanent: false,
+        direction: 'top',
+        opacity: 1
+      })
+      .addTo(this.map);
+    });
+
+  // Création des exutoires
+  gdf_stations_filter.forEach(outlet => {
+    
+    const fillColor = '#000000';
+
+    // Création d'un marqueur circulaire pour chaque exutoire
+    const marker = L.circleMarker([outlet.y_outlet, outlet.x_outlet], {
+      radius: 2,           // Taille du cercle
+      fillColor: fillColor, // Couleur de remplissage (noir)
+      color: fillColor,     // Couleur de bordure (noir)
+      weight: 0,            // Épaisseur de la bordure
+      opacity: 0,           // Opacité de la bordure
+      fillOpacity: 0.8      // Opacité du remplissage
+    }).addTo(this.map);      // Ajouter le marqueur à la carte
+  });
+
+  }
+}
