e1fafe4ce947199ac152ba27dcf890259161c253
mompm <monabudeau@gmail.com>
2024-09-26 13:45:52 +0200

    corrections documentations front

--- frontEnd/front/src/app/model/parameters-group.ts
+++ frontEnd/front/src/app/model/parameters-group.ts
@@ -1,14 +1,11 @@
 // Array<string> instead of string because it caused some display bug
-
 export interface ParametersGroup {
-    // parent?: ParametersGroup,
     name: Array<string>,
     Parameter?: Array<Parameter>,
     ParametersGroup?: Array<ParametersGroup>
 }
 
 export interface Parameter{
-    // parent?: ParametersGroup,
     name: Array<string>,
     description: Array<string>,
     possible_values: Array<string>,
@@ -17,9 +14,3 @@ export interface Parameter{
     default_value: Array<string>
 }
 
-// export function getFullPathParam(param: ParametersGroup | Parameter | undefined): Array<string> {
-// 	if(param === undefined) {
-// 		return [];
-// 	}
-// 	return [...getFullPathParam(param.parent), ...param.name];
-// }
